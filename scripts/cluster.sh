2eff725a3ccdee67a9e81627437b0c228e3453bf
Guillem-cirrus <guillemram97@gmail.com>
2024-03-25 17:05:01 +0000

    Update from cirrus

--- scripts/cluster.sh
+++ scripts/cluster.sh
@@ -1,7 +1,11 @@
-export PART=csd3
-export DATA_PATH=/rds/user/cs-rami1/rds-t2-cs119/guillem/datasets/data
+export PART=cirrus
 export NEPTUNE_API_TOKEN="eyJhcGlfYWRkcmVzcyI6Imh0dHBzOi8vYXBwLm5lcHR1bmUuYWkiLCJhcGlfdXJsIjoiaHR0cHM6Ly9hcHAubmVwdHVuZS5haSIsImFwaV9rZXkiOiI5OGI3M2NhOS0yZjYyLTQxMTktODYzYS03ZGNiNzAyMzM3OTEifQ=="
 export NEPTUNE_PROJECT=cache/train-template
-export TRANSFORMERS_CACHE=/rds/user/cs-rami1/hpc-work/.cache/transformers
-export HF_MODULES_CACHE=/rds/user/cs-rami1/hpc-work/.cache/transformers
-export HF_HOME=/rds/user/cs-rami1/hpc-work/.cache/transformers
+export PROJECT_NAME_NEPTUNE=$NEPTUNE_PROJECT
+export DATA_PATH=/work/sc126/sc126/gramirez/data/
+export API_TOKEN_NEPTUNE=$NEPTUNE_API_TOKEN
+export TRANSFORMERS_CACHE=/work/sc126/sc126/gramirez/.cache/huggingface
+export HF_HOME=$TRANSFORMERS_CACHE
+export XDG_HOME=$TRANSFORMERS_CACHE
+
+#/work/sc126/sc126/gramirez/miniconda3/cache/bin/python
