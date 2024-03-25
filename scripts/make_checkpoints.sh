60885eecba75727970d1adc40039ceb7ed3efd72
Guillem Ramirez <guillemram97@gmail.com>
2024-03-25 16:17:29 +0000

    Update from csd3

--- scripts/make_checkpoints.sh
+++ scripts/make_checkpoints.sh
@@ -1,14 +1,16 @@
-for TASK in fever openbook isear rt-polarity cr sst2
+for TASK in babi_qa natural_qa wikifact
 do
-    for MODEL in Mistral-Instruct-4b #Llama-7b-4b Llama-13b-4b llama-1-30b-4b falcon-40b-4b #llama-1-65b-4b Llama-70b-4b
-    do
-        for SEED in 0
-        do
-            export TASK_NAME=${TASK}_prob_${MODEL}
-            export SEED
-            PART=csd3
-            export PART
-            sbatch --export=ALL scripts/sub_$PART.sh
-        done
-    done
+    #for MODEL in Mixtral-Instruct-4b Mistral-Instruct-4b #Llama-7b-4b Llama-13b-4b llama-1-30b-4b falcon-40b-4b #llama-1-65b-4b Llama-70b-4b
+    #do
+     #   for SEED in 0
+     #   do
+	export TASK_NAME=${TASK}
+	export SEED=0
+	PART=csd3
+	export PART
+	sbatch --export=ALL scripts/sub_$PART.sh
+        #done
+   # done
+    #export TASK_NAME=${TASK}_prob_${MODEL}
+    #sbatch --export=ALL scripts/sub_$PART.sh
 done
